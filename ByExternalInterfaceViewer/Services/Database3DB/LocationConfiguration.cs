9c4df1def581935f1d944f9d229d8d8e1dab9ec7
Muschio11 <m.sorbo@icloud.com>
2026-03-13 18:20:41 +0100

    Revert "Location configuration completed"
    This reverts commit a499a72a334023214510d8c73c70156bd394bdde.

--- ByExternalInterfaceViewer/Services/Database3DB/LocationConfiguration.cs
+++ ByExternalInterfaceViewer/Services/Database3DB/LocationConfiguration.cs
@@ -1,23 +1,10 @@
-﻿using ByExternalInterfaceViewer.Models.Database3DBModels;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ByExternalInterfaceViewer.Services.Database3DB;
-
-public class LocationConfiguration : IEntityTypeConfiguration<LocationModel>
+namespace ByExternalInterfaceViewer.Services.Database3DB
 {
-    public void Configure(EntityTypeBuilder<LocationModel> entity)
+    class LocationConfiguration
     {
-        entity.ToTable("Locazioni");
-
-        entity.Property(e => e.LocationID)
-            .HasColumnName("LocazioneGUID");
-            
-        entity.Property(e => e.LocationType)
-            .HasColumnName("TipoLocazione")
-            .HasMaxLength(50);
     }
 }
