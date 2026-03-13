a499a72a334023214510d8c73c70156bd394bdde
Muschio11 <m.sorbo@icloud.com>
2026-03-13 18:19:49 +0100

    Location configuration completed

--- ByExternalInterfaceViewer/Services/Database3DB/LocationConfiguration.cs
+++ ByExternalInterfaceViewer/Services/Database3DB/LocationConfiguration.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using ByExternalInterfaceViewer.Models.Database3DBModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ByExternalInterfaceViewer.Services.Database3DB
+namespace ByExternalInterfaceViewer.Services.Database3DB;
+
+public class LocationConfiguration : IEntityTypeConfiguration<LocationModel>
 {
-    class LocationConfiguration
+    public void Configure(EntityTypeBuilder<LocationModel> entity)
     {
+        entity.ToTable("Locazioni");
+
+        entity.Property(e => e.LocationID)
+            .HasColumnName("LocazioneGUID");
+            
+        entity.Property(e => e.LocationType)
+            .HasColumnName("TipoLocazione")
+            .HasMaxLength(50);
     }
 }
