77a2b876d0a377c9c2c72bd136564f3c6442d453
Muschio11 <m.sorbo@icloud.com>
2026-03-13 17:02:02 +0100

    Cassette content list model completed

---
+++ ByExternalInterfaceViewer/Models/ExternalinterfaceDBModels/CassetteContentsListModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByExternalInterfaceViewer.Models.ExternalinterfaceDBModels;
+
+public class CassetteContentsListModel
+{
+    public int SheetGUID { get; set; }
+    public string MaterialName { get; set; }
+    public string MaterialDescription { get; set; }
+    public double Lenght { get; set; } 
+    public double Width { get; set; }
+    public double Thickness { get; set; }
+    public int Quantity { get; set; }
+    public string? SheetType { get; set; }
+    public string? CuttingPlan { get; set; }
+    public string? Description { get; set; }
+    public string? DocumentName { get; set; }
+    public string? SupplierName { get; set; }
+    public int CassetteID { get; set; }
+    public string? Status { get; set; }
+    public int LocationID { get; set; }
+    public string? Icon { get; set; }
+    public string? AdditionalField1 { get; set; }
+    public string? AdditionalField2 { get; set; }
+    public string? AdditionalField3 { get; set; }
+    public string? AdditionalField4 { get; set; }
+    public string? AdditionalField5 { get; set; }
+    public string? AdditionalField6 { get; set; }
+    public string? AdditionalField7 { get; set; }
+    public string? AdditionalField8 { get; set; }
+    public string? AdditionalField9 { get; set; }
+    public string? AdditionalField10 { get; set; }
+    public DateTime DataOperationTime { get; set; }
+}
