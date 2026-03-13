089bff4791796d161eafc9fe11dd045d16bd30e2
Muschio11 <m.sorbo@icloud.com>
2026-03-13 17:12:28 +0100

    Movement list model completed

--- ByExternalInterfaceViewer/Models/ExternalinterfaceDBModels/MovementsListModel.cs
+++ ByExternalInterfaceViewer/Models/ExternalinterfaceDBModels/MovementsListModel.cs
@@ -2,9 +2,38 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ByExternalInterfaceViewer.Models.ExternalinterfaceDBModels
+namespace ByExternalInterfaceViewer.Models.ExternalinterfaceDBModels;
+
+public class MovementsListModel
 {
-    internal class MovementsListModel
-    {
-    }
+    public long OperationID { get; set; }
+    public DateTime OperationTime { get; set; }
+    public string MaterialName { get; set; }
+    public string MaterialDescription { get; set; }
+    public double Lenght { get; set; }
+    public double width { get; set; }
+    public double Thickness { get; set; }
+    public int Quantity { get; set; }
+    public string? SheetType { get; set; }
+    public string? CuttingPlan { get; set; }
+    public string? Description { get; set; }
+    public string? DocumentName { get; set; }
+    public string? SupplierName { get; set; }
+    public int CassetteID { get; set; }
+    public string? Status { get; set; }
+    public int LocationID { get; set; }
+    public string? additionalField1 { get; set; }
+    public string? additionalField2 { get; set; }
+    public string? additionalField3 { get; set; }
+    public string? additionalField4 { get; set; }
+    public string? additionalField5 { get; set; }
+    public string? additionalField6 { get; set; }
+    public string? additionalField7 { get; set; }
+    public string? additionalField8 { get; set; }
+    public string? additionalField9 { get; set; }
+    public string? additionalField10 { get; set; }
+    public string? Icon { get; set; }
+    public long SheetGUID { get; set; }
+    public short Elab { get; set; }
+    public DateTime DataElab { get; set; }
 }
