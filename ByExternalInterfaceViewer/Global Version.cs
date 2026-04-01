64be430f6e135172e9b9dd3fcfecac566ff50961
Muschio11 <m.sorbo@icloud.com>
2026-04-01 17:41:08 +0200

    Create queries to get data of movement list table

--- ByExternalInterfaceViewer/Global Version.cs
+++ ByExternalInterfaceViewer/Global Version.cs
@@ -8,6 +8,6 @@ namespace ByExternalInterfaceViewer
     public  class GlobalVersion() 
     {
         
-        public string Version { get; set; } = "1.6.3";
+        public string Version { get; set; } = "1.7.3";
     }
 }
--- ByExternalInterfaceViewer/ViewModels/MovementsListViewModel.cs
+++ ByExternalInterfaceViewer/ViewModels/MovementsListViewModel.cs
@@ -11,14 +11,20 @@ namespace ByExternalInterfaceViewer.ViewModels;
 
 public partial class MovementsListViewModel: ObservableObject
 {
-   private readonly IDbContextFactory<AppDbContextExternalInterface> _dbContextFactory;
+   private readonly IDbContextFactory<AppDbContextExternalInterface> _dbContextFactoryExtInt;
+    private readonly IDbContextFactory<AppDBContextDatabase3> _dbContextFactoryDb3;
+
     [ObservableProperty]
     private ObservableCollection<MovementsListModel> _movements = new ();
+    
+    
 
-    public MovementsListViewModel(IDbContextFactory<AppDbContextExternalInterface> dbContextFactory)
+    public MovementsListViewModel(IDbContextFactory<AppDbContextExternalInterface> dbContextFactoryExtInt, IDbContextFactory<AppDBContextDatabase3> dbContextFactoryDb3)
     {
-        _dbContextFactory = dbContextFactory;
-        _=GetMovementsAsync();
+        _dbContextFactoryExtInt = dbContextFactoryExtInt;
+        _dbContextFactoryDb3 = dbContextFactoryDb3;
+
+        _ =GetMovementsAsync();
     }
 
 
@@ -26,12 +32,51 @@ public partial class MovementsListViewModel: ObservableObject
     {
         try
         {
-            await using var context = await _dbContextFactory.CreateDbContextAsync();
+            await using var contextExtInt = await _dbContextFactoryExtInt.CreateDbContextAsync();
+            await using var contextDb3 = await _dbContextFactoryDb3.CreateDbContextAsync();
 
             //var movements = await context.Locations.AsNoTracking().ToListAsync();
             Movements.Clear();
+            
+            //Query of single Database to perform join
+            var queryMovementList = await contextExtInt.MovementsList.OrderByDescending(m=>m.OperationID).Take(100).AsNoTracking().ToListAsync();
+            var queryLocations = await contextDb3.Locations.Where(l=>l.LocationID != 0 && l.LocationType != "magazzino").AsNoTracking().ToListAsync();
 
-            var query = await context.MovementsList.OrderByDescending(m=>m.OperationID).Take(100).AsNoTracking().ToListAsync();
+            var query = queryMovementList.Join(queryLocations,m=> m.LocationID, l => l.LocationID, (m, l) => new MovementsListModel
+            {
+                OperationID = m.OperationID,
+                OperationTime = m.OperationTime,
+                MaterialName = m.MaterialName,
+                MaterialDescription = m.MaterialDescription,
+                Length = m.Length,
+                Width = m.Width,
+                Thickness = m.Thickness,
+                Quantity = m.Quantity,
+                SheetType = m.SheetType,
+                CuttingPlan = m.CuttingPlan,
+                Description = m.Description,
+                DocumentName = m.DocumentName,
+                SupplierName = m.SupplierName,
+                CassetteID = m.CassetteID,
+                Status = m.Status,
+                LocationID = m.LocationID,
+                LocationName = l.LocationType,
+                AdditionalField1 = m.AdditionalField1,
+                AdditionalField2 = m.AdditionalField2,
+                AdditionalField3 = m.AdditionalField3,
+                AdditionalField4 = m.AdditionalField4,
+                AdditionalField5 = m.AdditionalField5,
+                AdditionalField6 = m.AdditionalField6,
+                AdditionalField7 = m.AdditionalField7,
+                AdditionalField8 = m.AdditionalField8,
+                AdditionalField9 = m.AdditionalField9,
+                AdditionalField10 = m.AdditionalField10,
+                Icon = m.Icon,
+                SheetGUID = m.SheetGUID,
+                Elab = m.Elab,
+                DateElab = m.DateElab
+            }).ToList();
+            
 
             Movements = new ObservableCollection<MovementsListModel>(query);
 
